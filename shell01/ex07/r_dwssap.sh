59c62baf96dc2e7b8b420e28cff731b6ab5fdf59
Youssef Bouryal <ybouryal@e2r11p2.1337.ma>
2024-08-27 17:59:45 +0100

    finished

--- shell01/ex07/r_dwssap.sh
+++ shell01/ex07/r_dwssap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat /etc/passwd | grep -v "#" | cut -d':' -f1 | rev
+cat /etc/passwd | grep -v "^#" | awk 'NR != 1 {print $0}' | cut -d':' -f1 | rev | sort -r | sed -n $FT_LINE1\,$FT_LINE2'p' | tr '\n' ',' | sed 's/,$/\n/' | sed 's/,/, /g'
