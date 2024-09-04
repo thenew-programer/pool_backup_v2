e709d1e3626b7de6b44c0cb0b24d3cd893200699
Youssef Bouryal <ybouryal@e2r11p2.1337.ma>
2024-09-04 16:31:23 +0100

    Shell01 - fix ex07

--- shell01/ex07/r_dwssap.sh
+++ shell01/ex07/r_dwssap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat /etc/passwd | grep -v "^#" | sed -n "p;n" | cut -d : -f 1 | rev | sort -r | awk "NR>=$FT_LINE1 && NR<=$FT_LINE2" | tr "\n" "," | sed 's/,/, /g' | sed 's/, $/./'
+cat /etc/passwd | grep -v "^#" | sed -n 'n;p' | cut -d ':' -f 1 | rev | sort -r | sed -n $FT_LINE1,$(echo $FT_LINE2)p | tr '\n' ', ' | sed 's/,$/./' | tr -d '\n'
