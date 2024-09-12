ceb2165a8c62e900cc0ee0581027fbc0aa07ac38
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-09-12 01:49:30 +0100

    C08 - Done

--- C08/ex04/ft_strs_to_tab.c
+++ C08/ex04/ft_strs_to_tab.c
@@ -12,72 +12,50 @@
 
 #include <stdlib.h>
 
-int	ft_strlen(char *str)
-{
-	int	i;
+int ft_strlen(char *str) {
+  int i;
 
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
+  i = 0;
+  while (str[i])
+    i++;
+  return (i);
 }
 
-char	*ft_strdup(char *src)
-{
-	char	*dest;
-	int		i;
-	int		src_len;
+char *ft_strdup(char *src) {
+  char *dest;
+  int i;
+  int src_len;
 
-	src_len = ft_strlen(src);
-	dest = (char *)malloc((sizeof(char) * src_len) + 1);
-	if (!dest)
-		return (NULL);
-	i = 0;
-	while (i < src_len + 1)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	return (dest);
+  src_len = ft_strlen(src);
+  dest = (char *)malloc(sizeof(char) * (src_len + 1));
+  if (!dest)
+    return (NULL);
+  i = 0;
+  while (i < src_len + 1) {
+    dest[i] = src[i];
+    i++;
+  }
+  return (dest);
 }
 
-struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
-{
-	int					i;
-	struct s_stock_str	*stock;
+struct s_stock_str *ft_strs_to_tab(int ac, char **av) {
+  int i;
+  t_stock_str *stock;
 
-	if (ac < 0 || !av || !*av)
-		return (NULL);
-	i = 0;
-	stock = (struct s_stock_str *)malloc(sizeof(struct s_stock_str) * ac);
-	if (!stock)
-		return (NULL);
-	while (i < ac)
-	{
-		stock[i].size = ft_strlen(av[i]);
-		stock[i].str = ft_strdup(av[i]);
-		stock[i].copy = ft_strdup(av[i]);
-		i++;
-	}
-	return (stock);
+  if (ac < 0 || !av || !*av)
+    return (NULL);
+  i = 0;
+  stock = (t_stock_str *)malloc(sizeof(t_stock_str) * (ac + 1));
+  if (!stock)
+    return (NULL);
+  while (i < ac) {
+    stock[i].size = ft_strlen(av[i]);
+    stock[i].str = av[i];
+    stock[i].copy = ft_strdup(av[i]);
+    i++;
+  }
+  stock[ac].size = 0;
+  stock[ac].str = 0;
+  stock[ac].copy = 0;
+  return (stock);
 }
-/**
-#include <stdio.h>
-int main(int argc, char **argv)
-{
-	int	i;
-	t_stock_str *stock;
-
-	i = 0;
-	stock = ft_strs_to_tab(argc, argv);
-	while (i < argc)
-	{
-		printf("|\t{size}: %d\n", stock[i].size);
-		printf("   -----{str}: %s\n", stock[i].str);
-		printf("|\t{copy}: %s\n\n", stock[i].copy);
-		i++;
-	}
-	i = 0;
-	free(stock);
-	return (0);
-}*/
--- C08/ex05/ft_show_tab.c
+++ C08/ex05/ft_show_tab.c
@@ -12,45 +12,37 @@
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
+void ft_putchar(char c) { write(1, &c, 1); }
+
+void ft_putstr(char *str) {
+  int i;
+
+  i = 0;
+  while (str[i])
+    ft_putchar(str[i++]);
 }
 
-void	ft_putstr(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		ft_putchar(str[i++]);
+void ft_putnbr(int nbr) {
+  if (nbr < 0) {
+    ft_putchar('-');
+    nbr *= -1;
+  }
+  if (nbr / 10)
+    ft_putnbr(nbr / 10);
+  ft_putchar((nbr % 10) + '0');
 }
 
-void	ft_putnbr(int nbr)
-{
-	if (nbr < 0)
-	{
-		ft_putchar('-');
-		nbr *= -1;
-	}
-	if (nbr / 10)
-		ft_putnbr(nbr / 10);
-	ft_putchar((nbr % 10) + '0');
-}
+void ft_show_tab(struct s_stock_str *par) {
+  int i;
 
-void	ft_show_tab(struct s_stock_str *par)
-{
-	int	i;
-
-	i = 0;
-	while (par[i].str)
-	{
-		ft_putnbr(par[i].size);
-		ft_putchar('\n');
-		ft_putstr(par[i].str);
-		ft_putchar('\n');
-		ft_putstr(par[i].copy);
-		ft_putchar('\n');
-		i++;
-	}
+  i = 0;
+  while (par[i].size != 0 && !par[i].str) {
+    ft_putstr(par[i].str);
+    ft_putchar('\n');
+    ft_putnbr(par[i].size);
+    ft_putchar('\n');
+    ft_putstr(par[i].copy);
+    ft_putchar('\n');
+    i++;
+  }
 }
