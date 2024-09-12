e2f2e48f9d4554108309f562c87b45177d7a48a5
Youssef Bouryal <ybouryal@e2r11p2.1337.ma>
2024-09-12 13:55:23 +0100

    C08 - Fixed

--- C08/ex01/ft_boolean.h
+++ C08/ex01/ft_boolean.h
@@ -11,20 +11,20 @@
 /* ************************************************************************** */
 
 #ifndef FT_BOOLEAN_H
-#define FT_BOOLEAN_H
+# define FT_BOOLEAN_H
 
-#include <unistd.h>
+# include <unistd.h>
 
-#define TRUE 1
-#define FALSE 0
+# define TRUE 1
+# define FALSE 0
 
-#define SUCCESS FALSE
+# define SUCCESS FALSE
 
-#define EVEN(x) ((x) % 2)
+# define EVEN(x) ((x) % 2)
 
-#define EVEN_MSG "I have an even number of arguments.\n"
-#define ODD_MSG "I have an odd number of arguments.\n"
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
 
-typedef int t_bool;
+typedef int	t_bool;
 
 #endif /* FT_BOOLEAN_H */
--- C08/ex04/ft_strs_to_tab.c
+++ C08/ex04/ft_strs_to_tab.c
@@ -6,56 +6,61 @@
 /*   By: ybouryal <ybouryal@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 02:56:52 by ybouryal          #+#    #+#             */
-/*   Updated: 2024/09/10 03:04:08 by ybouryal         ###   ########.fr       */
+/*   Updated: 2024/09/12 13:17:21 by ybouryal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "ft_stock_str.h"
 
-int ft_strlen(char *str) {
-  int i;
+int	ft_strlen(char *str)
+{
+	int	i;
 
-  i = 0;
-  while (str[i])
-    i++;
-  return (i);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
 
-char *ft_strdup(char *src) {
-  char *dest;
-  int i;
-  int src_len;
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+	int		src_len;
 
-  src_len = ft_strlen(src);
-  dest = (char *)malloc(sizeof(char) * (src_len + 1));
-  if (!dest)
-    return (NULL);
-  i = 0;
-  while (i < src_len + 1) {
-    dest[i] = src[i];
-    i++;
-  }
-  return (dest);
+	src_len = ft_strlen(src);
+	dest = (char *)malloc(sizeof(char) * (src_len + 1));
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (i < src_len + 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
 }
 
-struct s_stock_str *ft_strs_to_tab(int ac, char **av) {
-  int i;
-  t_stock_str *stock;
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	int			i;
+	t_stock_str	*stock;
 
-  if (ac < 0 || !av || !*av)
-    return (NULL);
-  i = 0;
-  stock = (t_stock_str *)malloc(sizeof(t_stock_str) * (ac + 1));
-  if (!stock)
-    return (NULL);
-  while (i < ac) {
-    stock[i].size = ft_strlen(av[i]);
-    stock[i].str = av[i];
-    stock[i].copy = ft_strdup(av[i]);
-    i++;
-  }
-  stock[ac].size = 0;
-  stock[ac].str = 0;
-  stock[ac].copy = 0;
-  return (stock);
+	if (ac < 0 || !av || !*av)
+		return (NULL);
+	i = 0;
+	stock = (t_stock_str *)malloc(sizeof(t_stock_str) * (ac + 1));
+	if (!stock)
+		return (NULL);
+	while (i < ac)
+	{
+		stock[i].size = ft_strlen(av[i]);
+		stock[i].str = av[i];
+		stock[i].copy = ft_strdup(av[i]);
+		i++;
+	}
+	stock[ac].size = 0;
+	stock[ac].str = 0;
+	stock[ac].copy = 0;
 }
