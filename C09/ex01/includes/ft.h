d5c6a71b7e6a6f8ce7d20bc0e136a1959d3768d5
Youssef Bouryal <ybouryal@e2r11p2.1337.ma>
2024-09-14 10:28:12 +0100

    C09 - ex01 files

---
+++ C09/ex01/includes/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybouryal <ybouryal@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/14 10:18:54 by ybouryal          #+#    #+#             */
+/*   Updated: 2024/09/14 10:18:55 by ybouryal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_swap(int *a, int *b);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_strlen(char *str);
+
+#endif
